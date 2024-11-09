56564215a5e15e66b61e8d07578468064543dbef
Tobias Rojahn <tobias.rojahn@gmail.com>
2024-11-09 11:10:00 +0100

    add bard and enemy

---
+++ scripts/bard.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@export var file: Resource
+@onready var sprite = $AnimatedSprite2D
+@onready var label: RichTextLabel = $RichTextLabel
+
+var data: Dictionary
+var word: String
+var available = true
+
+func trigger():
+	sprite.animation = "attack"
+	word = data["typo"].pick_random()
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	data = Common.load_data(file)
+	word = data["typo"].pick_random()
+	label.text = word
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
