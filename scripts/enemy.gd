4b0ac3b109615e7d28f26d2cfb5b6c3880a5943b
Tobias Rojahn <tobias.rojahn@gmail.com>
2024-11-09 17:53:37 +0100

    add icon active states

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,12 +4,12 @@ extends Node2D
 @onready var icons: GridContainer = $Icons
 
 
-var difficulty = 8
-var units = {
-	"Archer": load("res://assets/icons/archer.png"), 
-	"Warrior": load("res://assets/icons/warrior.png"), 
-	"Wizard": load("res://assets/icons/wizard.png"), 
-	"Bard": load("res://assets/icons/bard.png")
+var difficulty: int   = 8
+var units: Dictionary = {
+	"Archer": [load("res://assets/icons/archer.png"),load("res://assets/icons/archer_active.png")], 
+	"Warrior": [load("res://assets/icons/warrior.png"),load("res://assets/icons/warrior_active.png")], 
+	"Wizard": [load("res://assets/icons/wizard.png"), load("res://assets/icons/wizard_active.png")], 
+	"Bard": [load("res://assets/icons/bard.png"), load("res://assets/icons/bard_active.png")]
 }
 
 var weaknesses: Array[Dictionary] = []
@@ -21,20 +21,24 @@ func _ready() -> void:
 		var icon = load("res://scenes/entity/icon.tscn").instantiate()
 		
 		weaknesses.append({"unit": unit, "icon": icon })
-		icon.texture = units[unit]
+		icon.texture = units[unit][0]
 		icons.add_child(icon)
+		
+	weaknesses[0]["icon"].texture = units[weaknesses[0]["unit"]][1]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
 func harm(type: String):
-	if type == weaknesses[0]["unit"]:
+	if len(weaknesses) > 0 and type == weaknesses[0]["unit"]:
 		var w = weaknesses.pop_front()
 		w["icon"].queue_free()
 		
-	if len(weaknesses) < 1:
-		pass #victory
+		if len(weaknesses) < 1:
+			pass #victory
+		else:
+			weaknesses[0]["icon"].texture = units[weaknesses[0]["unit"]][1]
 
 func attack():
 	sprite.animation = "attack"
