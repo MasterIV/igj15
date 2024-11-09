7aa8d14e88f269613fad37ce750b5f67ee49d4ab
Felix Wagner <armag3ddon@web.de>
2024-11-09 14:04:05 +0100

    Add main menu scene

---
+++ scenes/menu.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$MainMenu/StartButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_credits_button_pressed() -> void:
+	$InfoText.newText("Credits:
+created @IGJam15, 2024
+
+Anna Leonhart (CEO)
+Judith Gastell (Head of 2D)
+Tamara Meyendriesch (Expert of 2D)
+Tobias Rojahn (Expert Senior Dev)
+Felix Wagner (Expert Dev)
+Marcel Zons (Expert Designer)
+Manuel Kuczynski (Expert Motion Designer)")
